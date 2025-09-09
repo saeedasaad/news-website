5f3c274ca29fd2978142e849ae3170877a99eb9b
Saeeda <saeeda123@gmail.com>
2025-09-09 15:36:00 +0500

    Initial commit: News website backend and frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import favicon from './assets/favicon.png';
-
-const link = document.createElement('link');
-link.rel = 'icon';
-link.type = 'image/png';
-link.href = favicon;
-document.head.appendChild(link);
-
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
