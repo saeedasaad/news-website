87c80b6795f959c5445f159d1b7108108486d7c3
Saeeda <saeeda123@gmail.com>
2025-09-09 16:35:53 +0500

    Initial commit: News website backend and frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server:{port: 5174}
+  server: { port: 5174 }
 });
