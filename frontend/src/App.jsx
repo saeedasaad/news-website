12f9c736e741e6a5cc0474b99d115fd390c7f103
Saeeda <saeeda123@gmail.com>
2025-09-09 16:05:36 +0500

    Initial commit: News website backend and frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import NewsApp from './components/Newsapp';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <>
-      <NewsApp/>
+    <Routes>
+        <Route path='/' element={<Home />} />
+    </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NewsApp from '../components/Newsapp';
+
+export default function Home() {
+  return (
+    <>
+    <NewsApp/>
+      
+    </>
+  );
+}
