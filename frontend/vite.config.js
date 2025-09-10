be205dbb8d7f2b0d187e2fed2db131d24c15e5a0
Saeeda <saeeda123@gmail.com>
2025-09-10 14:14:56 +0500

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, 
-  },
-  preview: {
-    port: 5173, 
-  },
-  build: {
-    outDir: "dist",
-  },
-  base: "./", 
+  server: {port: 5173}
 });
